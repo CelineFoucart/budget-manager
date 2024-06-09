980f37d6cf159d535204058e433556b1f7581594
Céline Foucart <celinefoucart@yahoo.fr>
2024-06-09 14:30:23 +0200

    add: sort field

--- src/views/BudgetMonitoring.vue
+++ src/views/BudgetMonitoring.vue
@@ -95,12 +95,42 @@
                     <table class="table table-bordered">
                         <thead>
                             <tr>
-                                <th class="text-gray">Date</th>
-                                <th class="text-gray">Libellé</th>
-                                <th class="text-gray">Catégorie</th>
-                                <th class="text-gray">Débit</th>
-                                <th class="text-gray">Crédit</th>
-                                <th class="text-gray">Solde</th>
+                                <th class="text-gray" @click="sortField = 'date'" role="button" :class="{'text-primary': sortField === 'date' }">
+                                    Date
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up-alt" viewBox="0 0 16 16">
+                                        <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5"/>
+                                    </svg>
+                                </th>
+                                <th class="text-gray" @click="sortField = 'title'" role="button" :class="{'text-primary': sortField === 'title' }">
+                                    Libellé
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up-alt" viewBox="0 0 16 16">
+                                        <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5"/>
+                                    </svg>
+                                </th>
+                                <th class="text-gray" @click="sortField = 'category'" role="button" :class="{'text-primary': sortField === 'category' }">
+                                    Catégorie
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up-alt" viewBox="0 0 16 16">
+                                        <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5"/>
+                                    </svg>
+                                </th>
+                                <th class="text-gray" @click="sortField = 'amount'" role="button" :class="{'text-primary': sortField === 'amount' }">
+                                    Débit
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up-alt" viewBox="0 0 16 16">
+                                        <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5"/>
+                                    </svg>
+                                </th>
+                                <th class="text-gray" @click="sortField = 'credit'" role="button" :class="{'text-primary': sortField === 'credit' }">
+                                    Crédit
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up-alt" viewBox="0 0 16 16">
+                                        <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5"/>
+                                    </svg>
+                                </th>
+                                <th class="text-gray" @click="sortField = 'sold'" role="button" :class="{'text-primary': sortField === 'sold' }">
+                                    Solde
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up-alt" viewBox="0 0 16 16">
+                                        <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5"/>
+                                    </svg>
+                                </th>
                                 <th class="text-gray">Vérifié</th>
                                 <th class="text-gray d-print-none">Actions</th>
                             </tr>
@@ -228,7 +258,8 @@ export default {
             openDeleteModal: false,
             loading: false,
             showTable: true,
-            showStats: false
+            showStats: false,
+            sortField: 'date'
         }
     },
 
@@ -264,6 +295,16 @@ export default {
                 }
             });
 
+            if (this.sortField === 'title' || this.sortField === 'category') {
+                return this.sortByText(records);
+            } else if (this.sortField === 'amount') {
+                return records.sort((a, b) => a.amount - b.amount);
+            } else if (this.sortField === 'credit') {
+                return records.sort((a, b) => b.amount - a.amount);
+            } else if (this.sortField === 'sold') {
+                return records.sort((a, b) => b.sold - a.sold);
+            }
+            
             return records;
         }
     },
@@ -284,6 +325,33 @@ export default {
     },
 
     methods: {
+        sortByText(records) {
+            records.sort((a, b) => {
+                let nameA = null;
+                let nameB = null;
+                if (this.sortField === 'title') {
+                    nameA = a.title;
+                    nameB = b.title;
+                } else {
+                    nameA = this.recordStore.categories[a.category] ? this.recordStore.categories[a.category].name : null;
+                    nameB = this.recordStore.categories[b.category] ? this.recordStore.categories[b.category].name : null;
+                }
+
+                nameA = nameA !== null ? nameA.toUpperCase() : nameA;
+                nameB = nameB !== null ? nameB.toUpperCase() : nameB;
+                
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            return records;
+        },
+
         async getRecords() {
             const date = new Date(this.date.year, this.date.month, 1);
             const minDate = dayjs(date).startOf('month').format('YYYY-MM-DD');
